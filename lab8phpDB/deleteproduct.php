a77e4bcf60b988825fd46c57eaad4c4ef39ef7c9
fermfirxzer <106776702+fermfirxzer@users.noreply.github.com>
2024-09-25 02:05:31 +0700

    Add templete

---
+++ lab8phpDB/deleteproduct.php
@@ -0,0 +1,10 @@
+<?php include "connect.php" ?>
+<?php
+$stmt = $pdo->prepare("DELETE FROM product WHERE pid=?");
+$stmt->bindParam(1, $_GET["pid"]);
+if ($stmt->execute()){
+header("location:Allproduct.php");
+}else{
+    echo "Try again!";
+}
+?>
